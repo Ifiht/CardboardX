5c41a27d614567562215794fb442576ee5bba462
Peter Pan <peter@never.lan>
2024-09-04 17:05:43 -0400

    add project files

---
+++ src/main/java/org/evlis/cardboardX/CardboardX.java
@@ -0,0 +1,17 @@
+package org.evlis.cardboardX;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CardboardX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
